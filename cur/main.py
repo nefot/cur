ebc0d4d197439f3c226d95f9e9fd4c9a9f2fd704
artyom <artyom27000@mail.ru>
2024-03-28 17:00:53 +0300

    V2

--- cur/main.py
+++ cur/main.py
@@ -7,10 +7,27 @@ import pandas as pd
 
 
 class DataReader:
-    _xls: object = pd.ExcelFile('C:\\Users\\artyom\\cur\\cur\\data.xlsx')
+    _xls: object = pd.ExcelFile('C:\\Users\\artyom\\cur\\cur\\data_mod.xlsx')
     _data: dict = pd.read_excel(_xls, sheet_name=None)
 
-    def __init__(self, name_region: str = 'Ростовская область', visualization=False):
+    def __init__(
+            self,
+            name_region: str = 'Ростовская область',
+            visualization=False,
+            directory='data_mod.xlsx',
+            parser="NEW",
+            debug=False):
+        """
+        :param parser:какую версию парсера использовать
+        :param name_region: имя региона/название таблицы
+        :param visualization: нужно ли визуализировать данные
+        :param directory: директория объекта
+        """
+        self.debug = debug
+        self.parser = parser
+        self._xls: object = pd.ExcelFile(directory)
+        self._data: dict = pd.read_excel(self._xls, sheet_name=None)
+
         self.name_region: str = name_region
         self._data_dict: dict = {}
         self.vis = visualization
@@ -124,13 +141,36 @@ class DataReader:
                         values[i] = next_val
         return data_dict
 
-    def normalization(self):
-        pass
+    @staticmethod
+    def normalize_data(data_dict):
+        normalized_dict = {}
+        for key, values in data_dict.items():
+            max_val = max(values)
+            min_val = min(values)
+            # Check if min_val and max_val are equal to avoid division by zero
+            if min_val != max_val:
+                normalized_values = [(val - min_val) / (max_val - min_val) for val in values]
+            else:
+                # If min_val and max_val are equal, set all normalized values to 0 or some default value
+                normalized_values = [0] * len(values)  # or any default value you prefer
+            normalized_dict[key] = normalized_values
+        return normalized_dict
 
     def multivisualization(self) -> object:
         pass
 
-    def genDict(self) -> dict:
+    def genDictNew(self) -> dict:
+        result_dict = {}
+        first_cell_dict = {}
+        for sheet_name, df in self._data.items():
+            for index, row in df.iterrows():
+                key = f"{sheet_name}_{index + 1}"  # Увеличиваем индекс на 1, чтобы он начинался с 1
+                values = row.values.tolist()
+                result_dict[key] = values[1:]  # Сохраняем все значения строки, начиная со второй ячейки
+                first_cell_dict[key] = values[0]  # Сохраняем значение первой ячейки
+        return result_dict, first_cell_dict
+
+    def genDictOld(self) -> dict:
         """
         генерирует словарь по exel файлу
         :return: dict{
@@ -191,30 +231,65 @@ class DataReader:
     @staticmethod
     def outDict(dicts: dict) -> print():
         for i, x in enumerate(dicts):
-            print(x, dicts[x])
+            print(x, str(dicts[x]).replace('\n', ''))
         print(dicts)
 
     def run(self) -> dict:
-        dicts = self.convert_to_float64(self.filter_data(self.genDict()))
-        # dicts = self.interpolate_missing_values(dicts)
-        self.outDict(dicts)
+        data_num, data_str = self.genDictNew()
+        if self.debug:
+            self.outDict(data_num)
+            print(25 * '-')
+            self.outDict(data_str)
+            print('')
 
-        print(self.describe_value_types(dicts))
-        if self.vis:
+        dicts = self.convert_to_float64(self.filter_data(data_num))
+
+        # dicts = self.normalize_data(dicts)
+        # dicts = self.interpolate_missing_values(dicts)
+
+        if self.debug:
+            print(self.describe_value_types(dicts))
+        if self.vis and (self.parser == "OLD"):
             self.visualize_and_save_images(dicts)
+        elif self.vis and (self.parser == "NEW"):
+            self.visualize_and_save_images2(dicts)
         return dicts
+
+    def visualize_and_save_images2(self, data_dict):
+        # Создаем папку "img", если она не существует
+        if not os.path.exists("img"):
+            os.makedirs("img")
+
+        # Создаем общий график для всех данных
+        fig, ax = plt.subplots(figsize=(19, 16))
+
+        # Проходимся по каждой группе и добавляем данные на график
+        for key, values in data_dict.items():
+            years = list(range(2016, 2016 + len(values)))
+            ax.plot(years, values, linestyle='-', label=key)
+
+        ax.set_title('Группы данных')
+        ax.set_xlabel('Год')
+        ax.set_ylabel('Значение')
+        ax.grid(True)
+
+        # Сохраняем изображение в папку "img"
+        file_path = os.path.join("img", "all_groups.png")
+        plt.savefig(file_path)
+        # Закрываем текущее изображение
+        plt.close(fig)
 
 
 def deletion(data):
     pass
 
 
-def median(dictonary):
+def median():
     pass
 
 
 if __name__ == '__main__':
-    rostov = DataReader(visualization=True)
+    rostov = DataReader(visualization=True, debug=True)
     russia = DataReader(name_region="Российская Федерация", visualization=True)
     ufu = DataReader(name_region="Южный федеральный округ", visualization=True)
 
